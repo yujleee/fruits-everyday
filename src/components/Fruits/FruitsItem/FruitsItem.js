a352c6cddb2f7e0cc8c8ebb62fba0c82feb8869d
이유정 <yujlee242@gmail.com>
2022-10-05 14:09:58 +0900

    🚧 FruitsItem 컴포넌트 수정

--- src/components/Fruits/FruitsItem/FruitsItem.js
+++ src/components/Fruits/FruitsItem/FruitsItem.js
@@ -1,21 +1,37 @@
-import FruitsItemForm from './FruitsItemForm';
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../../store/cart-slice';
 import classes from './FruitsItem.module.css';
 
 const FruitsItem = ({ id, name, description, price, image }) => {
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addToCart({
+        id,
+        name,
+        price,
+        image,
+        quantity: 1,
+      })
+    );
+  };
+
   return (
-    <div className={classes['fruits-item']} area-label="fruits-item">
+    <li className={classes['fruits-item']} area-label="fruits-item">
       <div className={classes.img} aria-label="image">
         <img src={image} alt={name} />
       </div>
       <h4>{name}</h4>
       <p>{description}</p>
       <p classes={classes['fruit-price']}>
-        <b>{price}원</b>
+        <b>{price.toLocaleString()} 원</b>
       </p>
       <div>
-        <FruitsItemForm />
+        <button onClick={addToCartHandler}>구매하기</button>
       </div>
-    </div>
+    </li>
   );
 };
 
