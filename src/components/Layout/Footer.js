78061f85a1c5e949c3bf6abd84ae27f9ff34e22d
이유정 <yujlee242@gmail.com>
2022-10-07 21:46:12 +0900

    ✨ 푸터 컴포넌트 추가

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <h2>매일과일</h2>
+      <p>ⓒ2022.매일과일.All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
