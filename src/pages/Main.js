60e7fd3ba57c2ac798738b091dfdef8f320c9450
이유정 <yujlee242@gmail.com>
2022-10-09 22:11:38 +0900

    ♻️ Main 페이지 컴포넌트 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,9 @@
-import { useSelector } from 'react-redux';
-
-import Header from '../components/Layout/Header';
 import Banner from '../components/Layout/Banner';
 import Fruits from '../components/Fruits/Fruits';
-import Cart from '../components/Cart/Cart';
 
 const Main = () => {
-  const modal = useSelector((state) => state.ui.visibleModal);
-
   return (
     <>
-      {modal && <Cart />}
-      <Header />
       <Banner />
       <Fruits />
     </>
