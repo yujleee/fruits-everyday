6aaade165df730c6409148bd42ac0ff9eb080914
이유정 <yujlee242@gmail.com>
2022-10-10 22:14:47 +0900

    🚸  헤더 로고 링크 추가

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
@@ -7,9 +8,11 @@ const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1 className={classes.logo}>
-          <img src="/images/logo.png" alt="매일과일" />
-        </h1>
+        <Link to={'/'}>
+          <h1 className={classes.logo}>
+            <img src="/images/logo.png" alt="매일과일" />
+          </h1>
+        </Link>
         <HeaderCartButton />
       </header>
     </Fragment>
